a1e5231ecd03697b35b14e2eba906e56f429f6d6
Umar <f2018266207@umt.edu.pk>
2022-04-05 14:42:46 +0500

    first commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final int d = 300;
-     final String a = "days";
+    final int d = 30;
+    final String a = "days";
     return Scaffold(
       appBar: AppBar(
-        title: Text("Slight try"),
+        title: Text("Mobile Application"),
       ),
       body: Center(
         child: Container(
-          child: Text("Flutter developer $d $a  "),
+          child: Text(
+            "Flutter developer $d $a  ",
+            style: TextStyle(
+                color: Colors.red, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
       drawer: Drawer(),
-      
-      
     );
-    
   }
 }
